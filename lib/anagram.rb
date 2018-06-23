94a105cb4c90f085fa224c3aeaeb4249bfd8fa93
Krishan Jethwa <krishan.jethwa@student.manchester.ac.uk>
2018-06-23 22:20:22 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -5,6 +5,25 @@ class Anagram
     @word = word
   end
 
+  def match(array) #array is the list of words which could be an anagrams
+    matches = []
+    split_word = @word.split("")
+    array.each do |anagram| #gets each word in the list
+      split_anagram = anagram.split("")
+      counter = 0 #checking each word
+      split_anagram.each do |letter|
+        if split_word.include?(letter)
+          split_word -= [letter]
+          counter += 1
+        end
+      end
+      if count == @word.length
+        matches << anagram
+      end
+    end
+    return matches
+  end
+
   def match(array)
     matches = []
     letters = @word.split("")
