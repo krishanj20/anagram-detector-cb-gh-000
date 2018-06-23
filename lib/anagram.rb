62eaa343654a255143a949bdd5ae6e604ed3fa11
Krishan Jethwa <krishan.jethwa@student.manchester.ac.uk>
2018-06-23 21:17:44 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,31 @@
 # Your code goes here!
+class Anagram
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(array)
+    matches = []
+    letters = @word.split
+    array.each do |word|
+      letters_a = word.split
+      counter = 0
+      letters_a.each do |letter|
+        if letters.include?(letter)
+          letters -= [letter]
+          counter += 1
+          puts counter
+        end
+      if counter == letter.size
+        matches << word
+        counter = 0
+      else
+        counter = 0
+      end
+    end
+  end
+  matches
+end
+
+end
